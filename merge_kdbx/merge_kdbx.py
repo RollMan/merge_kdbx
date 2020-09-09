2e75b775342dcf586d5acdc65df6e94e07ec7f67
Yohei Shimmyo <qwpmb554@gmail.com>
2020-09-09 16:28:37 +0900

    complete coding. ready to test.

--- merge_kdbx/merge_kdbx.py
+++ merge_kdbx/merge_kdbx.py
@@ -6,7 +6,6 @@ import merge_kdbx.kdbx_xml_util
 import datetime
 from copy import deepcopy
 
-DATETIME_FORMAT = "%Y-%m-%dT%H-%M-%SZ"
 
 def main():
     try:
@@ -22,32 +21,26 @@ def main():
 
     # root.Root.Group.Entry.{Times.LastModificationTime, String.Password, String.Title}
     for target_xml_root in src_xml_roots[1:].find('Root'):
-        for group in target_xml_root.findall('Group'):
-            for entry in group.findall('Entry'):
-                    group_name = group.find('Name').text
-                    entry_last_mod = datetime.datetime.strptime(entry.find('Times').find('LastModificationTime').text, DATETIME_FORMAT)
-                    attr = {'Password': '', 'Title': ''}
-                    for s in entry.findall('String'):
-                        for k in attr.keys():
-                            if s.find('Key').text == k:
-                                attr[k] = s.find('Value').text
+        retrieve_and_update_entries(target_xml_root, dst.find('Root'))
 
-                    # Check `dst` if it has the group of `group_name` and same entry including attr['Title'].
-                    dst_group = merge_kdbx.kdbx_xml_util.find_group(dst.find('Root'), group_name)
-                    if dst_group == None:
-                        dst.find('Root').append(group)
-
-                    dst_entry = merge_kdbx.kdbx_xml_util.find_entry_by_title(dst_group, attr['Title'])
-                    if dst_entry == None:
-                        dst.find('Root').find(group_name).append(entry)
-                    else:
-                        src_last_mod = entry_last_mod
-                        dst_last_mod = datetime.datetime.strptime(dst_entry.find('Times').find('LastModificationTime').text, DATETIME_FORMAT)
-                        if src_last_mod > dst_last_mod:
-                            # I believe `dst_entry` is a reference and modification here affects to the entry in `dst_tree` :X.
-                            dst_entry = deepcopy(entry)
     
     dst_tree.write(args.dst)
 
 if __name__ == '__main__':
     main()
+
+def retrieve_and_update_entries(src_elem, dst_elem):
+    # start with root.Root
+    for src_child_entry in src_elem.findall('Entry'):
+        entry_title = src_child_entry.find('Title')
+        dst_child_entry = merge_kdbx.kdbx_xml_util.find_entry_by_title(dst_elem, entry_title)
+        merge_kdbx.kdbx_xml_util.update_dst_entry(src_child_entry, dst_child_entry, dst_elem)
+    
+    for src_child_group in src_elem.findall('Group'):
+        group_name = src_child_group.find('Name')
+        dst_child_group = merge_kdbx.kdbx_xml_util.find_group(dst_elem, group_name)
+        if dst_child_group == None:
+            merge_kdbx.kdbx_xml_util.update_dst_group(src_child_group, dst_elem)
+        else:
+            # dst and src have the same group. retrieve the child group
+            retrieve_entries(src_child_group, dst_elem)
